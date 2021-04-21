555670f1bc17257eb437a9464ae89ecca5c0c70d
Duocnguyeng3 <duocnguyen.g3@gmail.com>
2021-04-21 19:43:21 +0700

    finish movie view ui  1st time

--- src/js/controller.js
+++ src/js/controller.js
@@ -19,6 +19,9 @@ import 'core-js/stable';
 //multi search
 // &language=vi-VN
 
+// fetch cast & crew
+// https://api.themoviedb.org/3/movie/550/credits?api_key=e616cd78f7df65758287d12dbeae7d4a
+
 // const getMovie = async function () {
 //     // const res = await fetch('https://api.themoviedb.org/3/movie/13002?api_key=e616cd78f7df65758287d12dbeae7d4a');
 //     // const res = await fetch('https://api.themoviedb.org/3/configuration?api_key=e616cd78f7df65758287d12dbeae7d4a');
@@ -35,21 +38,21 @@ const resultsContainer = document.querySelector('.results')
 const movieContainer = document.querySelector('.movie')
 
 const testObject = [
-    { "number": 1, "name": 'Nick' },
-    { "number": 2, "name": 'Judy' },
-    { "number": 3, "name": 'David' }
+  { "number": 1, "name": 'Nick' },
+  { "number": 2, "name": 'Judy' },
+  { "number": 3, "name": 'David' }
 ];
 const id = 2;
 const test = testObject.map(ele => ele.number = id ? ele.name : '');
 
 const getGenre = function (id) {
-    const name = movieGenres.find(ele => ele.id === id).name;
-    return name;
+  const name = movieGenres.find(ele => ele.id === id).name;
+  return name;
 }
 
 // src="https://image.tmdb.org/t/p/w92${result.poster_path}"
 const generateMarkupResult = function (result) {
-    return `
+  return `
     <li class="preview">
     <a class="preview__link" href="#${result.id}">
       <figure class="preview__figure">
@@ -66,8 +69,8 @@ const generateMarkupResult = function (result) {
     `
 }
 
-const generateMarkupDetail = function (data) {
-    return `
+const generateMarkupMovie = function (data) {
+  return `
   <figure class="movie__fig">
   <img src="https://image.tmdb.org/t/p/w780${data.backdrop_path}" alt="Tomato" class="movie__img" />
   <h1 class="movie__title">
@@ -88,30 +91,30 @@ const generateMarkupDetail = function (data) {
 
 
 const getSearchResult = async function (e) {
-    e.preventDefault();
-    const query = document.querySelector('.search__field').value;
-    const res = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`);
-    const data = await res.json();
-    const results = data.results;
-    const markup = results.map(generateMarkupResult).join('');
+  e.preventDefault();
+  const query = document.querySelector('.search__field').value;
+  const res = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`);
+  const data = await res.json();
+  const results = data.results;
+  const markup = results.map(generateMarkupResult).join('');
 
-    resultsContainer.innerHTML = '';
-    resultsContainer.insertAdjacentHTML('afterbegin', markup);
+  resultsContainer.innerHTML = '';
+  resultsContainer.insertAdjacentHTML('afterbegin', markup);
 }
 
 const getMovie = async function () {
-    const id = window.location.hash.slice(1);
-    if (!id) return;
-    const res = await fetch(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
-    const data = await res.json();
-    const markup = generateMarkupDetail(data);
+  const id = window.location.hash.slice(1);
+  if (!id) return;
+  const res = await fetch(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
+  const data = await res.json();
+  const markup = generateMarkupMovie(data);
 
-    movieContainer.innerHTML = '';
-    movieContainer.insertAdjacentHTML('afterbegin', markup);
+  movieContainer.innerHTML = '';
+  movieContainer.insertAdjacentHTML('afterbegin', markup);
 }
 
 const addHandlerRender = function () {
-    ['hashchange', 'load'].forEach(e => window.addEventListener(e, getMovie));
+  ['hashchange', 'load'].forEach(e => window.addEventListener(e, getMovie));
 }
 addHandlerRender();
 
@@ -139,23 +142,23 @@ search.addEventListener('submit', getSearchResult);
 // genres ID
 
 const movieGenres = [
-    { "id": 28, "name": "Action" },
-    { "id": 12, "name": "Adventure" },
-    { "id": 16, "name": "Animation" },
-    { "id": 35, "name": "Comedy" },
-    { "id": 80, "name": "Crime" },
-    { "id": 99, "name": "Documentary" },
-    { "id": 18, "name": "Drama" },
-    { "id": 10751, "name": "Family" },
-    { "id": 14, "name": "Fantasy" },
-    { "id": 36, "name": "History" },
-    { "id": 27, "name": "Horror" },
-    { "id": 10402, "name": "Music" },
-    { "id": 9648, "name": "Mystery" },
-    { "id": 10749, "name": "Romance" },
-    { "id": 878, "name": "Science Fiction" },
-    { "id": 10770, "name": "TV Movie" },
-    { "id": 53, "name": "Thriller" },
-    { "id": 10752, "name": "War" },
-    { "id": 37, "name": "Western" }
+  { "id": 28, "name": "Action" },
+  { "id": 12, "name": "Adventure" },
+  { "id": 16, "name": "Animation" },
+  { "id": 35, "name": "Comedy" },
+  { "id": 80, "name": "Crime" },
+  { "id": 99, "name": "Documentary" },
+  { "id": 18, "name": "Drama" },
+  { "id": 10751, "name": "Family" },
+  { "id": 14, "name": "Fantasy" },
+  { "id": 36, "name": "History" },
+  { "id": 27, "name": "Horror" },
+  { "id": 10402, "name": "Music" },
+  { "id": 9648, "name": "Mystery" },
+  { "id": 10749, "name": "Romance" },
+  { "id": 878, "name": "Science Fiction" },
+  { "id": 10770, "name": "TV Movie" },
+  { "id": 53, "name": "Thriller" },
+  { "id": 10752, "name": "War" },
+  { "id": 37, "name": "Western" }
 ]
